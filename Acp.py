67a1e626e116f991b64b22ecba06e2cb8b8ef963
aaravhajare <hajareaarav66@gmail.com>
2025-01-27 21:18:50 +0530

    odd and even

--- Acp.py
+++ Acp.py
@@ -1,1 +1,6 @@
-#printing 
+A = int(input("Enter a number: "))  # Convert input to an integer
+
+if A % 2 == 0:  # Check if the number is divisible by 2
+    print("The number is even.")
+else:
+    print("The number is odd.")
